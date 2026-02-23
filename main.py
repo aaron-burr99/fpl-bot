325eb70e8f49d63455af0d8506cb47792c8d2dac
wangsiyang68 <siyang.official@gmail.com>
2026-02-23 08:33:59 +0800

    feat: use mysql db operations

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import requests
 from dotenv import load_dotenv
 import os
 from datetime import datetime, timezone, timedelta
+import json
+
+from _mysql_helpers import create_database_and_table, update_table, get_table
 
 load_dotenv()
 
@@ -36,20 +39,24 @@ def get_last_updated_time():
     except:
         print('error in retrieving time')
 
+def dict_hash(d):
+    serialized = json.dumps(d, sort_keys=True).encode()
+    return hash(serialized)
+
 async def send_message_if_got_updates(context: ContextTypes.DEFAULT_TYPE):
-    last_updated_time = datetime.strptime(get_last_updated_time(), '%Y-%m-%dT%H:%M:%S%z')
-    now_utc = datetime.now(timezone.utc)
-    # now_utc = datetime.strptime("2026-02-13T01:37:41Z", '%Y-%m-%dT%H:%M:%S%z') #stubbed ver of time
-    delta = (now_utc - last_updated_time).total_seconds()
-    if delta < 10:
+    new_result = get_all_scores()
+    old_result = get_table()
+    # new_result['Siyang'] = 999 #test stub
+
+    if dict_hash(old_result) != dict_hash(new_result):
         try:
-            print(f"time difference is {delta} secs, there must be a new update...")
-            await send_message(context)
+            print(f"there must be a new update...")
+            await send_message(context, old_result, new_result)
+            update_table(new_result)
         except:
             print('got error trying to send message')
     else:
-        sg_time = now_utc + timedelta(hours=8)
-        print(f"at {sg_time}, time difference is {delta:.2f} secs, there is no new update...")
+        print(f"there is no new update...")
 
 def get_scores(team_id):
 
@@ -83,8 +90,21 @@ def get_scores(team_id):
     except (ValueError, KeyError) as e:
         print(f"Error parsing response: {e}")
 
+def get_all_scores():
+    result = {}
+    for name, team_id in team_ids.items():
+        result[name] = get_scores(team_id)
+    return result
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     chat_id = update.effective_chat.id
+
+    # Startup database if not already available
+    create_database_and_table()
+
+    # Retrieve the stats and save them into the database
+    result = get_all_scores()
+    update_table(result)
 
     # Check if the job already exists for this chat
     current_jobs = context.job_queue.get_jobs_by_name(str(chat_id))
@@ -93,18 +113,18 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         return
 
     # Add the new repeating job to the queue
-    # interval=10.0 specifies the interval in seconds
+    # interval=60.0 specifies the interval in seconds
     # first=0.0 means it starts immediately
     context.job_queue.run_repeating(
         send_message_if_got_updates,
-        interval=10.0,
+        interval=60.0,
         first=0.0,
         data=chat_id,
         name=str(chat_id)
     )
 
-    await update.message.reply_text("Who is going to treat Hai Di Lao?")
-    await show_option_buttons(update, context)
+    await update.message.reply_text("Polling every 1 min now, will update when scores change...")
+    # await show_option_buttons(update, context)
 
 async def show_option_buttons(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     keyboard = [
@@ -127,19 +147,48 @@ async def button_selection_handler(update: Update, context: ContextTypes.DEFAULT
     # await query.edit_message_text(f'You selected option: {query.data.split("_")[1]}')
 
 # Function that the JobQueue will call periodically
-async def send_message(context: ContextTypes.DEFAULT_TYPE):
+async def send_message(context: ContextTypes.DEFAULT_TYPE, old_result, new_result):
     """Send the periodic message."""
     # The chat_id is passed via the context.job.data in this example
     print("sending message...")
     chat_id = context.job.data
-    result = {}
-    for name, team_id in team_ids.items():
-        result[name] = get_scores(team_id)
+    sorted_result = sorted(new_result.items(), key=lambda item: item[1], reverse=True)
+    old_sorted_result = sorted(old_result.items(), key=lambda item: item[1], reverse=True)
 
-    sorted_result = sorted(result.items(), key=lambda item: item[1], reverse=True)
-    pretty_sorted = [f"{idx+1}. {name}: {points} pts" for idx, (name, points) in enumerate(sorted_result)]
+    new_rankings = {name: idx+1 for idx, (name, points) in enumerate(sorted_result)}
+    old_rankings = {name: idx+1 for idx, (name, points) in enumerate(old_sorted_result)}
+
+    pretty_sorted = []
+    for idx, (name, points) in enumerate(sorted_result):
+        if name in old_rankings:
+            rank_diff = old_rankings[name] - new_rankings[name]
+            if rank_diff > 0:
+                rank_emoji = "🔺"
+            elif rank_diff < 0:
+                rank_emoji = "🔻"
+            else:
+                rank_emoji = "➖"
+        else:
+            rank_emoji = "🆕"
+
+        if name in old_result:
+            diff = points - old_result.get(name, 0)
+            sign = "+" if diff >= 0 else ""
+            if diff:
+                entry = f"{idx+1}. {points} pts : {name}{rank_emoji}({sign}{diff})"
+            else:
+                entry = f"{idx+1}. {points} pts : {name}{rank_emoji} "
+        else:
+            entry = f"{idx+1}. {points} pts : {name:}{rank_emoji} (new entry)"
+
+        pretty_sorted.append(entry)
+
     sorted_items_desc = "\n".join(pretty_sorted)
-    await context.bot.send_message(chat_id=chat_id, text=f'{sorted_items_desc}')
+
+    # sorted_result = sorted(result.items(), key=lambda item: item[1], reverse=True)
+    # pretty_sorted = [f"{idx+1}. {name}: {points} pts" for idx, (name, points) in enumerate(sorted_result)]
+    # sorted_items_desc = "\n".join(pretty_sorted)
+    await context.bot.send_message(chat_id=chat_id, text=f'🚨UPDATE🚨\n{sorted_items_desc}')
 
 def main():
     application = Application.builder().token(API_TOKEN).build()
